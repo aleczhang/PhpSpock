f7d22635511e16563e60798ebf86b377a9316582
Alex Rudakov <alexandr.rudakov@modera.net>
2011-11-04 12:11:52 +0200

    More details in AssertionException

--- src/PhpSpock/Adapter/PhpUnitAdapter.php
+++ src/PhpSpock/Adapter/PhpUnitAdapter.php
@@ -26,4 +26,95 @@ class PhpUnitAdapter implements \PhpSpock\Adapter {
         }
 
     }
+
+    public static function isSpecification(\PHPUnit_Framework_TestCase $test)
+    {
+        if ($test->getName(false) === NULL) {
+            throw new \PHPUnit_Framework_Exception(
+              'PHPUnit_Framework_TestCase::$name must not be NULL.'
+            );
+        }
+
+        try {
+            $class  = new \ReflectionClass($test);
+            $method = $class->getMethod($test->getName(false));
+            $methodName = $method->getName();
+            return substr($methodName, -4) == 'Spec';
+
+        } catch (\ReflectionException $e) {
+            $test->fail($e->getMessage());
+        }
+    }
+
+    public static function runTest(\PHPUnit_Framework_TestCase $test)
+    {
+        if ($test->getName(false) === NULL) {
+            throw new \PHPUnit_Framework_Exception(
+              'PHPUnit_Framework_TestCase::$name must not be NULL.'
+            );
+        }
+
+        try {
+            $class  = new \ReflectionClass($test);
+            $method = $class->getMethod($test->getName(false));
+            $methodName = $method->getName();
+            return substr($methodName, -4) == 'Spec';
+
+        } catch (\ReflectionException $e) {
+            $test->fail($e->getMessage());
+        }
+
+        try {
+            $phpSpock = new \PhpSpock\PhpSpock();
+
+            $phpSpock->runWithAdapter(
+                new static(),
+                array($test, $methodName));
+        }
+        catch (\Exception $e) {
+            if (!$e instanceof \PHPUnit_Framework_IncompleteTest &&
+                !$e instanceof \PHPUnit_Framework_SkippedTest &&
+                is_string($test->getExpectedException()) &&
+                $e instanceof $test->getExpectedException()) {
+
+                $expectedException = self::getExpectedExceptionFromAnnotation($test, $methodName);
+                if (is_string($expectedException['message']) &&
+                    !empty($expectedException['message'])) {
+                    $test->assertContains(
+                      $expectedException['message'],
+                      $e->getMessage()
+                    );
+                }
+
+                if (is_int($expectedException['code']) &&
+                    $expectedException['code'] !== 0) {
+                    $test->assertEquals(
+                      $expectedException['code'], $e->getCode()
+                    );
+                }
+
+                $test->addToAssertionCount(1);
+
+                return;
+            } else {
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * @since  Method available since Release 3.4.0
+     */
+    protected static function getExpectedExceptionFromAnnotation($class, $methodName)
+    {
+        try {
+            return \PHPUnit_Util_Test::getExpectedException(
+              get_class($class), $methodName
+            );
+        }
+
+        catch (\ReflectionException $e) {
+        }
+        return null;
+    }
 }
