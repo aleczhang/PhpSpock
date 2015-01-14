f988cae0b935643d3840e3082727240fc9566fbf
zhangjun <zhangjun@tuniu.com>
2015-01-14 23:32:41 +0800

    Add PhpSpec for the base class of test case which uses PhpSpock.

---
+++ src/PhpSpock/PhpSpec.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of PhpSpock.
+ *
+ * PhpSpock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PhpSpock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PhpSpock.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2015 alec <jun850605@gmail.com>
+ *
+ **/
+
+namespace PhpSpock;
+
+/**
+ * Class PhpSpec
+ * @package PhpSpock
+ */
+abstract class PhpSpec extends \PHPUnit_Framework_TestCase {
+
+    protected function runTest() {
+        if (\PhpSpock\Adapter\PhpUnitAdapter::isSpecification($this)) {
+            return \PhpSpock\Adapter\PhpUnitAdapter::runTest($this);
+        } else {
+            return parent::runTest();
+        }
+    }
+}
